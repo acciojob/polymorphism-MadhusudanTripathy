6355b1bfc8b94dbd13d024e026b0c6627b37c76c
Madhusudan Tripathy <madhusudantripathy77@gmail.com>
2023-01-13 15:08:37 +0530

    poly assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,54 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x,int y){
 
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(1.3,1.2));
+    }
 }
+/*
+ * Task 1:
+ * create a class Product inside Main class
+ *
+ * Task 2:
+ * create object of Product in Main function called p
+ *
+ * Task 3:
+ * create a method of following defination
+ * public int product(int x, int y) {}
+ *
+ * call this method from Main using Product class object p
+ *
+ * Task 4:
+ * create a Overloaded method product of following defination
+ * public int product(int x, int y, int z) {}
+ *
+ * call this method also from Main using Product class object p
+ *
+ * Task 5:
+ * create a Overloaded method product of following defination
+ * public double product(double x, double y) {}
+ *
+ * call this method also from Main using Product class object p
+ *
+ * Observations:
+ * This class contains 3 classes with same name, but it complie & run successfully
+ *
+ */
